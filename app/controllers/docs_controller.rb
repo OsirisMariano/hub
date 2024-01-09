b55183d7a95d1b7a0ecf83aba4267dd8528f028c
Osiris Mariano <oslimacontato@gmail.com.br>
2024-01-09 20:00:29 -0300

    Update docs views and define actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,26 +3,23 @@ class DocsController < ApplicationController
   
   def index
     @docs = Doc.all.order("created_at DESC")
-  
   end
 
-  def show    
+  def show
   end
 
   def new
     @doc = Doc.new
-  
   end
 
   def create
     @doc = Doc.new(doc_params)
 
-    if @doc.after_save
-      redirect_to 'new'
+    if @doc.save
+      redirect_to @doc
     else
       render 'new'
     end
-    
   end
 
   def edit
@@ -30,21 +27,24 @@ class DocsController < ApplicationController
   end
 
   def update
-    
+    if @doc.update(doc_params)
+      redirect_to @doc
+      else
+        render 'edit'
+    end
   end
 
   def destroy
-  
+    @doc.destroy
+    redirect_to docs_path
   end
   
   private
     def find_doc
       @doc = Doc.find(params[:id])
-
     end
 
     def doc_params
-      params.require(:docs).permit(:title, :content)
+      params.require(:doc).permit(:title, :content)
     end
-
 end
