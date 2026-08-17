240fde382a8b0917446d45ac451d2ea19af4e758
Osiris Mariano <38112205+OsirisMariano@users.noreply.github.com>
2026-08-17 08:38:12 -0300

    Fix post-merge issues: schema.rb, ESBuild imports, test config
    - Fix schema.rb: replace broken UUID dump comments with proper bigint
      Active Storage table definitions (SQLite compatible)
    - Fix application.js: remove broken jquery/popper.js imports, delegate
      to bootstrap.js module
    - Fix welcome_controller_test: use root_url instead of nonexistent
      welcome_index_url
    - Disable sassc CSS compressor in test env (incompatible with
      Bootstrap 5.3 CSS custom properties)
    - Remove duplicate css process from Procfile.dev

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,1 @@
-// Importe as bibliotecas necessárias para o Bootstrap
-import 'jquery'
-import 'popper.js'
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import * as bootstrap from "bootstrap"
+import './bootstrap'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
+  config.assets.css_compressor = nil
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
